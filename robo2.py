ebad79f681999a1e1396c95d1bfdc16bbe5b6013
PedroLVF26 <130997142+PedroLVF26@users.noreply.github.com>
2023-04-24 14:16:44 -0300

    Add files via upload

---
+++ robo2.py
@@ -0,0 +1,37 @@
+ 2: r"""
+           0: {cabeça_nome}
+           Is available: {cabeça_status}
+           Attack: {cabeça_ataque}                              
+           Defense: {cabeça_defesa}
+           Energy consumption: {cabeça_energia_consumida}
+                           ^
+                           |                       |1: {arma_nome}
+                          ___                      |Is available: {arma_status}
+                         /[-])//  ___              |Attack: {arma_ataque}
+                    __ --\ `_/~--|  / \            |Defense: {arma_defesa}
+                  /_-/~~--~~ /~~~\\_\ /\           |Energy consumption: {arma_energia_consumida}
+                  |  |___|===|_-- | \ \ \        
+_/~~~~~~~~|~~\,   ---|---\___/----|  \/\-\             |2: {braço_esquerdo_nome}
+~\________|__/   / // \__ |  ||  / | |   | |           |Is available: {braço_esquerdo_status}
+         ,~-|~~~~~\--, | \|--|/~|||  |   | |           |Attack: {braço_esquerdo_ataque}
+         [3-|____---~~ _--'==;/ _,   |   |_|           |Defense: {braço_esquerdo_defesa}
+                     /   /\__|_/  \  \__/--/           |Energy consumption: {braço_esquerdo_energia_consumida}
+                    /---/_\  -___/ |  /,--|    ------> |
+                    /  /\/~--|   | |  \///             |3: {braço_direito_nome}
+                   /  / |-__ \    |/                   |Is available: {braço_direito_status}
+                  |--/ /      |-- | \                  |Attack: {braço_direito_ataque}
+                 \^~~\\/\      \   \/- _               |Defense: {braço_direito_defesa}
+                  \    |  \     |~~\~~| \              |Energy consumption: {braço_direito_energia_consumida}
+                   \    \  \     \   \  | \      
+                     \    \ |     \   \    \              |4: {perna_esquerda_nome} 
+                       |~~|\/\|     \   \   |             |Is available: {perna_esquerda_status}
+                     |   |/         \_--_- |\             |Attack: {perna_esquerda_ataque}
+                     |  /            /   |/\/             |Defense: {perna_esquerda_defesa}
+                      ~~             /  /                 |Energy consumption: {perna_esquerda_energia_consumida}
+                                    |__/          ------> |
+                                                          |5: {perna_direita_nome}
+                                                          |Is available: {perna_direita_status}
+                                                          |Attack: {perna_direita_ataque}
+                                                          |Defense: {perna_direita_defesa}
+                                                          |Energy consumption: {perna_direita_energia_consumida}
+ """,
